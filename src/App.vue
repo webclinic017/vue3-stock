155ddfb3cccd561b1333c48a09226500ab18f8d4
seonk94 <seonk94@naver.com>
2020-06-06 22:46:33 +0900

    init #2

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
+      <router-link to="/login">Login</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
+<style lang='scss'>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
