02f1064c0127ff51b8896e9dd8e58db7fa8336bd
seonk94 <seonk94@naver.com>
2020-06-11 21:33:13 +0900

    feat: chart-js install

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container>
+    <v-layout></v-layout>
+  </v-container>
 </template>
 
 <script>
