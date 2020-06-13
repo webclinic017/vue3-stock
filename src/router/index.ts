3ee2e5f7c121d4de4678b6f007cea65abd433a20
seonk94 <seonk94@naver.com>
2020-06-13 15:18:46 +0900

    fix: router history mode set

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
 import * as NavigationGuardFunction from './NavigationGuard';
 
 
@@ -13,24 +14,17 @@ const routes: Array<RouteConfig> = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-  {
     path: '/login',
     name: 'Login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue'),
+    component: Login,
   },
 ];
 
 const router = new VueRouter({
+  mode: 'history',
   routes,
 });
 
