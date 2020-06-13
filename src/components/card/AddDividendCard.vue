59f1e4cfb88af77530669d0d68a9274b85ff5fe4
seonk94 <seonk94@naver.com>
2020-06-14 05:24:58 +0900

    feat: dividend card init

---
+++ src/components/card/AddDividendCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card outlined class="add-dividend-card">
+    <v-container class="card-container">
+      <v-row column wrap align="center" justify="center" class="card-row">
+        <div class>
+          <v-icon size="20">mdi-plus</v-icon>
+        </div>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script lang='ts'>
+import { Vue } from 'vue-property-decorator';
+
+export default Vue.extend({
+  name: 'DividendCard',
+  data: () => ({}),
+  props: {
+    symbol: String,
+    logo: String,
+  },
+  methods: {
+    addStock: function addStock(symbol: string) {
+      this.$emit('add', symbol);
+    },
+  },
+});
+</script>
+
+<style scoped lang='scss'>
+.add-dividend-card {
+  padding: 4px;
+  margin: 4px;
+  width: 200px;
+  height: 200px;
+
+  &:hover {
+    cursor: pointer;
+    background: #ccc;
+  }
+  .card-container {
+    width: 100%;
+    height: 100%;
+    .card-row {
+      height: 100%;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,45 @@
 <template>
-  <v-container>
-    <v-layout></v-layout>
+  <v-container fluid>
+    <v-row justify="center" class="home-layout">
+      <dividend-card
+        v-for="stock in stockList"
+        :key="stock"
+        :symbol="stock"
+        :logo="`https://storage.googleapis.com/iexcloud-hl37opg/api/logos/${stock}.png`"
+      />
+      <add-dividend-card @add="addStock" />
+    </v-row>
   </v-container>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+<script lang="ts">
+import { Vue } from 'vue-property-decorator';
+import DividendCard from '../components/card/DividendCard.vue';
+import AddDividendCard from '../components/card/AddDividendCard.vue';
 
-export default {
+export default Vue.extend({
   name: 'Home',
   components: {
-    HelloWorld,
+    DividendCard,
+    AddDividendCard,
   },
-};
+  data: () => ({
+    stockList: ['MSFT', 'T', 'MAIN', 'O', 'TSLA', 'KO', 'SPY'],
+  }),
+  methods: {
+    addStock: function addStock(symbol: string) {
+      this.stockList.push(symbol);
+    },
+    deleteStock: function deleteStock(symbol: string) {
+      this.stockList = this.stockList.filter(stock => stock !== symbol);
+    },
+  },
+});
 </script>
+
+<style scoped lang='scss'>
+.home-layout {
+  max-width: 1400px;
+  margin: auto;
+}
+</style>
