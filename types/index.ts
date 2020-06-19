4b958eb793086483b5e92ed04eb8205393da0a1b
seonk94 <seonk94@naver.com>
2020-06-19 21:36:03 +0900

    feat: symbol list set store, set interface

--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,17 @@ export interface StockDatum {
   symbol: string;
   holdings: number;
 }
+
+export interface IexSymbol {
+  symbol: string;
+  exchange: string;
+  name: string;
+  date: string;
+  type: string;
+  iexId: string;
+  region: string;
+  currency: string;
+  isEnabled: boolean;
+  figi: string;
+  cik: string;
+}
