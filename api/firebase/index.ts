3418443e3efda5f9e01cfdf6b4da3611a70b7e15
seonk94 <seonk94@naver.com>
2020-06-20 21:05:21 +0900

    feat: 추가할때 firestore 에 저장

--- api/firebase/index.ts
+++ api/firebase/index.ts
@@ -5,11 +5,15 @@ import { StockDatum } from '../../@types';
 export default class FirebaseClient {
 
 
-  public setStockDatum(id: string, symbol: string, data: StockDatum) {
-    return firebase.firestore().collection(id).doc(symbol).set(data);
+  public setStockDatum(id: string, data: StockDatum) {
+    return firebase.firestore().collection(id).doc(data.symbol).set(data);
   }
 
   public getStockDatum(id: string) {
     return firebase.firestore().collection(id).get()
+  }
+
+  public deleteStockDatum(id: string, symbol: string) {
+    return firebase.firestore().collection(id).doc(symbol).delete()
   }
 }
