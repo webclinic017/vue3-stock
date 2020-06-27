60a3e2ddee229142a3cc62a5989a2b43636787a4
seonk94 <seonk94@naver.com>
2020-06-27 11:10:23 +0900

    feat: bar chart init

---
+++ src/components/chartjs/BasicBarChart.vue
@@ -0,0 +1,43 @@
+<script>
+import { Bar } from 'vue-chartjs';
+
+export default {
+  extends: Bar,
+  props: ['data'],
+  watch: {
+    data() {
+      this.render();
+    },
+  },
+  mounted() {
+    this.render();
+  },
+  methods: {
+    render() {
+      this.renderChart({
+        labels: [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December',
+        ],
+        datasets: [
+          {
+            label: 'Dividends',
+            backgroundColor: '#f87979',
+            data: this.data,
+          },
+        ],
+      });
+    },
+  },
+};
+</script>
