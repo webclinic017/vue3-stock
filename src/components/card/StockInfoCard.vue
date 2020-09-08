3f8d4cf917c0ad43e866749eda4df93a8130796d
seonk94 <seonk94@naver.com>
2020-09-08 21:16:13 +0900

    feat: company info add

--- src/components/card/StockInfoCard.vue
+++ src/components/card/StockInfoCard.vue
@@ -4,6 +4,14 @@
       <h3>{{ stock.symbol }}</h3>
       <v-subheader>{{ stock.company.companyName}}</v-subheader>
     </v-card-title>
+    <v-card-text>
+      <v-list dense>
+        <v-list-item v-for="key of showKeys" :key="key">
+          <v-list-item-content>{{key}}</v-list-item-content>
+          <v-list-item-content>{{stock.company[key]}}</v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
   </v-card>
 </template>
 
@@ -14,6 +22,16 @@ import { StockDatum } from '@/@types';
 @Component({})
 export default class StockInfoCard extends Vue {
   @Prop() stock!: StockDatum;
+
+  private showKeys = [
+    'CEO',
+    'companyName',
+    'description',
+    'employees',
+    'exhange',
+    'sector',
+    'website',
+  ];
 }
 </script>
 
